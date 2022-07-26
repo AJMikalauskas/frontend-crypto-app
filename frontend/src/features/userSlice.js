3fa769c83d7d941475e067f64bd0493185eaa24c
AJMikalauskas <97716972+AJMikalauskas@users.noreply.github.com>
2022-07-26 09:55:25 -0500

    Had to fix error within package.json of the frontened: error name and fix: https://stackoverflow.com/questions/70374005/invalid-options-object-dev-server-has-been-initialized-using-an-options-object

---
+++ frontend/src/features/userSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    //name of this slice
+    name: "user",
+    // initial state of the reducer
+    initialState: {
+        value: []
+    },
+    //reducers for thsi specific slice, can be multiple slices and reducers within the slices
+    reducers: {
+        
+    }
+})
