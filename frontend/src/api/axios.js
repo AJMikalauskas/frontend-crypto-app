1a67d19b8d267744b9d06c416a1889089fd8f0a6
AJMikalauskas <97716972+AJMikalauskas@users.noreply.github.com>
2022-08-27 10:23:04 -0500

    Deployed Website, Some of the Final Code For Login Functionality To Work

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,14 +1,20 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:3500';
+const BASE_URL = 'https://crypto-app-demo-test.herokuapp.com/';
 
 export default axios.create({
-    baseURL: BASE_URL
+    baseURL: BASE_URL,
+    headers: {
+        'Access-Control-Allow-Origin' : '*'
+    }
 })
 
 // axios private to have interceptors, to work with JWT to refresh a token if our current accessToken or refreshToken is expired.
     // withCredentials is for the refreshToken cookie
 export const axiosPrivate = axios.create({
     baseURL: BASE_URL,
-    headers: { 'Content-Type': 'application/json'},
+    headers: { 
+        'Content-Type': 'application/json', 
+        'Access-Control-Allow-Origin' : '*'
+    },
     withCredentials: true
 })
